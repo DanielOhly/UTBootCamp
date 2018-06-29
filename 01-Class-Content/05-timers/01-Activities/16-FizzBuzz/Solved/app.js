6e62d3e49f0b6874b63928a180dc14921570aeae
Antonio Gage <antoniojgage@gmail.com>
2018-06-28 21:27:52 -0500

    Week 6 day 1 & week 5 solutions

---
+++ 01-Class-Content/05-timers/01-Activities/16-FizzBuzz/Solved/app.js
@@ -0,0 +1,17 @@
+// FizzBuzz Solution
+
+// =================
+
+function fizzBuzz() {
+
+  // Loop 100 times, starting from the number 1
+  var i = 1,
+    f = 'Fizz',
+    b = 'Buzz',
+    out = '';
+    
+  for (; i <= 100; i++) {
+    out = !(i % 3) ? !(i % 5) ? f + b : f : !(i % 5) ? b : i;
+    console.log(out);
+  }
+}
